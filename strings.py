ba8ec0ff0a299ed7fe6fe617fb395e2cb4639ba8
joyaljaimon <88231783+joyaljaimon@users.noreply.github.com>
2021-09-25 22:09:19 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,22 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Pythons string's are easy to use")
+print('We can even indicate "quotes" in strings')
+print("Hello" + " World")
+greeting = "Hello"
+name = "Joyal"
+print(greeting + name)
+
+# If we want a space,we can add that too
+print(greeting + ' ' + name)
+
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "14 years"
+print(name + " is " + age + "years old")
+print(type(age))
